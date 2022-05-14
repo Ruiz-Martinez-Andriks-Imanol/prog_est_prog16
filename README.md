3f314c6aad2dfc33cc2ed777f18aa58263a9f4cb
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 18:16:38 -0500

    prog_est_prog16
    Programa que pide las calificaciones de un alumno

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # prog_est_prog16
-Programa que pide la calificación de un alumno
+Autor : Andriks Imanol Ruiz Martínez, Fecha: 24/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de ciclos y condicionales en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras y flotantes</li>
+    <li>printf para mostrar varias variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerias</li>
+    <li>do-while</li>
+    <li>Contador</li>
+    <li>Condicionales</li>
+    <li>Comentarios para la doumentación interna del programa.</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Programa que pide las calificaciones de un alumno.
+<br>
+<br>
+<p>Breve descripción:
+Debemos crear un programa que pide las calificaciones de un alumno y realize lo siguiente:
+<ul>
+<li>Calcula su promedio</li>
+<li>Cuenta cunatas materias reprobo</li>
+<li>Cuenta cuantas materias aprobo</li>
+<li>Calcula su porcentaje de aprobación</li>	
+<li>Se terminara de pedir datos cuando se introduzca un numero negativo</li>
+<li>El programa deve validar que las calificaciones sean validas(0-10)</li>
+
+<br>
+</p>
+
---
+++ prog16.cpp
@@ -0,0 +1,54 @@
+
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 24/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa 
+Materia: Programación Estructrada 
+Ciclo: 01/2022
+
+Este es un programa de ciclos y condicionales en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  
+    -Variables enteras y flotantes
+    -printf para mostrar varias variables
+    -scanf
+    -El uso de include para las librerias
+    -do-while
+    -Contador
+    -Condicionales
+    -Comentarios para la doumentación interna del programa
+*/
+#include<stdio.h>
+int main(){
+	//Declaracion de variables
+	float cal, porcentaje_ap, prom, suma=0;
+	int i=0, aprobadas=0, reprobadas=0;
+	//Proceso
+	//Ciclo que prepite mientras que las calificaciones no sean negativas
+	do {
+		printf("Introduzca la calificacion: ");
+		scanf("%f",&cal);
+		//validacion de cal que sea entre 1-10
+		if (cal>=0 && cal<=10){
+			i++; //Cuenta el número de calificaciones validas
+			suma=suma+cal;
+			if (cal<7){
+				reprobadas++;
+			}
+			else {
+				aprobadas++;
+			}
+		}
+		else {
+			printf("Calificacion invalida!!!\n");
+		}
+	}while(cal>=0);
+	//Calculo de promedio y porcentaje de aprobacion, una vez que se introdujeron todas las calificaciones
+	prom=suma/i;
+	porcentaje_ap=(float)aprobadas/i*100.0;
+	//Salida
+	printf("Total de calificacion introducidas: %d\n",i);
+	printf("Promedio: %.1f\n",prom);
+	printf("Calificaciones aprobatorias: %d\n", aprobadas);
+	printf("Calificaciones reprobatorias: %d\n", reprobadas);
+	printf("Porcentaje de aprobacion: %.2f%%", porcentaje_ap);
+	return 0;
+}
